f161f0099acba3f9f99b122eb6a1e67535944ee8
uca-spider <ucaspider@gmail.com>
2024-11-20 20:59:22 +0700

    bismillah

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,4 +1,3 @@
-import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
@@ -11,17 +10,13 @@ import {
   CardsSkeleton,
 } from '@/app/ui/skeletons';
 import { Metadata } from 'next';
- 
+
 export const metadata: Metadata = {
   title: 'Dashboard',
 };
 
 export default async function Page() {
     const {
-      numberOfInvoices,
-      numberOfCustomers,
-      totalPaidInvoices,
-      totalPendingInvoices,
     } = await fetchCardData();
   return (
     <main>
